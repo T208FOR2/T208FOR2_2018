9e295af5a1c3c9efd09f222d82b3ae54bc077b8f
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-08-20 14:40:56 +0000

    autocommit

--- Lectures/Lecture_01_01/main.cpp
+++ Lectures/Lecture_01_01/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    int tala = 0;
+    string texti = "";
+
+    cin >> tala;
+    cin >> texti;
+
+    cout << tala < texti;
 
     return 0;
 }
