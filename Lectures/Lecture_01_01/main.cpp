badf6279d359e89fa27e8405b75a5138ee9a05fb
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-08-20 15:13:36 +0000

    autocommit

--- Lectures/Lecture_01_01/main.cpp
+++ Lectures/Lecture_01_01/main.cpp
@@ -5,8 +5,12 @@ using namespace std;
 
 int main()
 {
+    double pi = 3.14159259;
 
     cout << "Hello. \"This\" is the second line" << endl;
+
+    cout.precision(20);
+    cout << pi << endl;
 
     return 0;
 }
