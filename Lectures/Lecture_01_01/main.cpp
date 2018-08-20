56b18a6126ed3065fb1ebfd59a464087a44066d5
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-08-20 14:57:16 +0000

    autocommit

--- Lectures/Lecture_01_01/main.cpp
+++ Lectures/Lecture_01_01/main.cpp
@@ -5,12 +5,6 @@ using namespace std;
 
 int main()
 {
-    int tala = 0;
-
-    cout << "Skrifaðu tölu: ";
-    cin >> tala;
-
-    cout << "Talan i öðru veldi er: " << tala*tala << endl;
 
     return 0;
 }
