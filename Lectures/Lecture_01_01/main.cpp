4fea891ec07993e4a4a01605157f531ff87b28e2
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-08-20 15:21:49 +0000

    autocommit

--- Lectures/Lecture_01_01/main.cpp
+++ Lectures/Lecture_01_01/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
+
     double einhvertala = 0.0, onnurtala = 0.0;
 
-    cin >> einhvertala >> onnurtala;
+    cin >> pow(einhvertala, 3);
 
     cout.precision(20);
     cout << einhvertala << endl;
