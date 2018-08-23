cb65357986c0f1800e1e0d75862a784699fe16e6
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-08-23 10:01:06 +0000

    autocommit

--- Lectures/Lecture_02_01/main.cpp
+++ Lectures/Lecture_02_01/main.cpp
@@ -5,13 +5,17 @@ using namespace std;
 int main()
 {
     const int ITERATIONS = 20;
+    const string ERRORMESSAGE = "whoops!!!";
     int x;
     cin >> x;
+
+    if (x > ITERATIONS) {
+        cout << ERRORMESSAGE << endl;
+    }
 
     while (x <= ITERATIONS) {
         cout << x << endl;
         x++;
-        ITERATIONS++;
     }
 
     return 0;
