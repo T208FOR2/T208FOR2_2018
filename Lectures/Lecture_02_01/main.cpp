2e921923ab08660a3b4204a330dc0eeb0c3562f5
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-08-23 09:40:36 +0000

    autocommit

--- Lectures/Lecture_02_01/main.cpp
+++ Lectures/Lecture_02_01/main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main()
 {
+    int team = 1;
+
+    for (int i = 0; i < 20; i++) {
+        cout << "player " << i+1 << " in team " << team % 4<< endl;
+        team = team + 1;
+    }
 
 
 
