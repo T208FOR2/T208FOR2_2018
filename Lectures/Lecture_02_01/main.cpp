8ab7e617bc6e64c5c5af12373e58d6d05a722d67
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-08-23 09:15:55 +0000

    autocommit

--- Lectures/Lecture_02_01/main.cpp
+++ Lectures/Lecture_02_01/main.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main()
 {
-    double  a,b;
+    double a;
+    int b;
     cin >> a >> b;
 
     cout << a << " / " << b << " = " << a / b << endl;
-    //cout << a << " % " << b << " = " << a % b << endl;
+    cout << a << " % " << b << " = " << a % b << endl;
 
     return 0;
 }
