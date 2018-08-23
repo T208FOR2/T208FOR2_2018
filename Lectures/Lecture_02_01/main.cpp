c0300e64f2e2f57afe2b392040579338b992dfc3
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-08-23 09:42:40 +0000

    autocommit

--- Lectures/Lecture_02_01/main.cpp
+++ Lectures/Lecture_02_01/main.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main()
 {
-    int team = 1;
+    int team = 0;
 
     for (int i = 0; i < 20; i++) {
-        cout << "player " << i+1 << " in team " << team % 4<< endl;
+        cout << "player " << i+1 << " in team " << (team % 4) + 1 << endl;
         team = team + 1;
     }
 
