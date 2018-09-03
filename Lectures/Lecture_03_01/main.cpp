b29c6c95990c8d3fd9abe6eb5f170c18d5e688db
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-09-03 14:53:14 +0000

    autocommit

--- Lectures/Lecture_03_01/main.cpp
+++ Lectures/Lecture_03_01/main.cpp
@@ -4,18 +4,6 @@ using namespace std;
 
 int main()
 {
-    string something = "";
-
-    while (true) {
-        cin >> something;
-
-        if (something == "please") {
-            break;
-        }
-    }
-
-
-
 
     return 0;
 }
