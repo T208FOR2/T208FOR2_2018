416f3018253f043919d3c907cc764d1e30717e54
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-09-03 14:47:03 +0000

    autocommit

--- Lectures/Lecture_03_01/main.cpp
+++ Lectures/Lecture_03_01/main.cpp
@@ -7,9 +7,10 @@ int main()
     string nafn;
     cin >> nafn;
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 100; i < 3; i++) {
         cout << "Hi" << nafn << endl;
     }
+
 
     return 0;
 }
