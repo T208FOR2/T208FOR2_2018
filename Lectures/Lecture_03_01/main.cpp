612e1c717f1633cc327ef8376eb2b8299bcc7735
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-09-03 14:12:01 +0000

    autocommit

--- Lectures/Lecture_03_01/main.cpp
+++ Lectures/Lecture_03_01/main.cpp
@@ -4,13 +4,15 @@ using namespace std;
 
 int main()
 {
-
-    string nafn;
-    cin >> nafn;
-
     {
-        cout << "Hi " << nafn << endl;
+        string nafn;
+        cin >> nafn;
+        {
+            cout << "Hi " << nafn << endl;
+        }
+
     }
+
 
 
     return 0;
