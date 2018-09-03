f26b52f6f9b10a8ed4f4eb11ada059f7f21f9b37
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-09-03 15:21:47 +0000

    autocommit

--- Lectures/Lecture_03_01/main.cpp
+++ Lectures/Lecture_03_01/main.cpp
@@ -2,13 +2,15 @@
 
 using namespace std;
 
+double calculate_tax_tennessee(double salary);
+
 int main()
 {
-    int laun;
+    double laun;
     double tax_tennessee, tax_georgia, tax_something, tax_otherstate;
     cin >> laun;
 
-    tax_tennessee = laun * 0.4;
+    tax_tennessee = calculate_tax_tennessee(laun);
 
     if (laun < 100000) {
         tax_georgia = laun * 0.35;
