9880d5e18ac498313f4a9458186d525282a23dd2
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-09-03 15:17:43 +0000

    autocommit

--- Lectures/Lecture_03_01/main.cpp
+++ Lectures/Lecture_03_01/main.cpp
@@ -8,8 +8,14 @@ int main()
     double tax_tennessee, tax_georgia, tax_something, tax_otherstate;
     cin >> laun;
 
-    tax_tennessee
+    tax_tennessee = laun * 0.4;
 
+    if (laun < 100000) {
+        tax_georgia = laun * 0.35;
+    }
+    else {
+        tax_georgia = 100000*0.35 + (laun - 100000) * 0.7;
+    }
 
 
     return 0;
