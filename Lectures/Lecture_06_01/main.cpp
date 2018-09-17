ed72d04a97c5a1c4c9662a5debb8830a12d2ca4e
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-09-17 15:26:45 +0000

    autocommit

--- Lectures/Lecture_06_01/main.cpp
+++ Lectures/Lecture_06_01/main.cpp
@@ -6,36 +6,19 @@
 using namespace std;
 
 void open_file(ifstream& theinputfile, string thefilename);
-void open_file(ofstream& theoutputfile, string thefilename);
-
-void output(ostream& x, int totalsum, int counter);
 
 int main()
 {
-    int tala, totalsum = 0, counter = 0;
 
     ifstream theinputfile;
     ofstream theoutputfile;
 
-    open_file(theinputfile, "/Users/Eyjo/Desktop/randomtolur.txt");
-    open_file(theoutputfile, "/Users/Eyjo/Desktop/nidurstada.txt");
-
-    while (theinputfile >> tala) {
-        totalsum += tala;
-        counter++;
-    }
-
-    output( theoutputfile, totalsum, counter );
-    output( cout, totalsum, counter );
+    open_file(theinputfile, "/Users/Eyjo/Desktop/grimm.txt");
 
     theinputfile.close();
-    theoutputfile.close();
+
 
     return 0;
-}
-
-void output(ostream& x, int totalsum, int counter) {
-    x << "The average is: " << static_cast<double>(totalsum) / counter << endl;
 }
 
 
@@ -48,11 +31,3 @@ void open_file(ifstream& theinputfile, string thefilename) {
     }
 }
 
-void open_file(ofstream& theoutputfile, string thefilename) {
-    theoutputfile.open( thefilename.c_str() );
-
-    if ( theoutputfile.fail() ) {
-        cout << "Can't open the file: " << thefilename << endl;
-        exit(1);
-    }
-}
