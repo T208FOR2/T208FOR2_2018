92b5d51c89c4591951484a927776cc5f042fc638
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-09-17 14:39:39 +0000

    autocommit

--- Lectures/Lecture_06_01/main.cpp
+++ Lectures/Lecture_06_01/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
 
 using namespace std;
 
 int main()
 {
+    ifstream theinputfile;
+    //theinputfile.open("randomtolur.txt");
+    theinputfile.open("/Users/Eyjo/Desktop/randomtolur.txt");
+    //theinputfile.open("c:\\Users\\Eyjo\\Desktop\\randomtolur.txt"); // <-- WINDOWS
+
+    if ( theinputfile.fail() ) {
+        cout << "Can't open the file!!!" << endl;
+        exit(1);
+    }
+
+    // something clever!
+
+    theinputfile.close();
 
     return 0;
 }
