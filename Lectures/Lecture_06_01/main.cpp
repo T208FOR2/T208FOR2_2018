b002b2645920650b996d883a104e36ba6cd1496a
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-09-17 15:22:37 +0000

    autocommit

--- Lectures/Lecture_06_01/main.cpp
+++ Lectures/Lecture_06_01/main.cpp
@@ -8,6 +8,8 @@ using namespace std;
 void open_file(ifstream& theinputfile, string thefilename);
 void open_file(ofstream& theoutputfile, string thefilename);
 
+void output(ostream& x, int totalsum, int counter);
+
 int main()
 {
     int tala, totalsum = 0, counter = 0;
@@ -22,14 +24,20 @@ int main()
         totalsum += tala;
         counter++;
     }
+
+    output( theoutputfile, totalsum, counter );
+    output( cout, totalsum, counter );
+
     theinputfile.close();
-
-    theoutputfile << "The average is: " << static_cast<double>(totalsum) / counter << endl;
-
     theoutputfile.close();
 
     return 0;
 }
+
+void output(ostream& x, int totalsum, int counter) {
+    x << "The average is: " << static_cast<double>(totalsum) / counter << endl;
+}
+
 
 void open_file(ifstream& theinputfile, string thefilename) {
     theinputfile.open( thefilename.c_str() );
