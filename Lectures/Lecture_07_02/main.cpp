5c7a62cd1e21be53638080669047e06feabff829
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-09-20 09:32:01 +0000

    autocommit

--- Lectures/Lecture_07_02/main.cpp
+++ Lectures/Lecture_07_02/main.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main()
 {
+    int tolur[5];
+
+    for (int i = 0; i < 5; i++) {
+        tolur[i] = i;
+    }
+
+    for (int i = 0; i < 5; i++) {
+        cout << tolur[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
