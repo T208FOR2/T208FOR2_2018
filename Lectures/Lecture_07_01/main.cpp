670a5c7ddc0aa1788ca9c8d8df3fd0fe36b5fd4b
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-09-20 08:43:08 +0000

    autocommit

--- Lectures/Lecture_07_01/main.cpp
+++ Lectures/Lecture_07_01/main.cpp
@@ -5,8 +5,18 @@
 
 using namespace std;
 
+void open_file(ifstream& inputfile, string filename);
+
 int main()
 {
 
     return 0;
+}
+
+void open_file(ifstream& inputfile, string filename) {
+    inputfile.open(filename.c_str());
+    if (inputfile.fail()) {
+        cout << "Can't open the file: " << filename << endl;
+        exit(1);
+    }
 }
