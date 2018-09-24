6663c545e203e2c3721ba42c0ecd9bf7617a8707
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-09-24 14:07:01 +0000

    autocommit

--- Lectures/Lecture_08_01/main.cpp
+++ Lectures/Lecture_08_01/main.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+
 int main()
 {
+    int n;
+    cin >> n;
+
+    int tolur[ n ];
+    for (int i = 0; i < n; i++) {
+        cin >> tolur[i];
+    }
+
+    for (int i = n-1; i >= 0; i--) {
+        cout << tolur[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
