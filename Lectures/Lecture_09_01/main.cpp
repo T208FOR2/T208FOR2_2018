59a479eecf769da78e72a8f53c644c9d0e3f47c9
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-09-27 09:56:50 +0000

    autocommit

--- Lectures/Lecture_09_01/main.cpp
+++ Lectures/Lecture_09_01/main.cpp
@@ -1,29 +1,11 @@
 #include <iostream>
 #include <string>
-#include <cctype>
 
 using namespace std;
 
-string tolower(string s);
-
 int main() {
-    string s;
-    getline(cin,s);
 
-    cout << tolower(s) << endl;
-    cout << s << endl;
-
-    for (int i = 0; i < s.length(); i++) {
-        s[i] = toupper(s[i]);
-    }
-    cout << s << endl;
 
     return 0;
 }
 
-string tolower(string s) {
-    for (int i = 0; i < s.length(); i++) {
-        s[i] = tolower(s[i]);
-    }
-    return s;
-}
