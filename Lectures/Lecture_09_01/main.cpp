5a8d7d4ad0e315180b57f6d2c484c52fb32190d4
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-09-27 09:50:37 +0000

    autocommit

--- Lectures/Lecture_09_01/main.cpp
+++ Lectures/Lecture_09_01/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 
 using namespace std;
 
@@ -7,8 +8,14 @@ int main() {
     string s;
     cin >> s;
 
-    s[10] = 'X';
+    for (int i = 0; i < s.length(); i++) {
+        s[i] = tolower(s[i]);
+    }
+    cout << s << endl;
 
+    for (int i = 0; i < s.length(); i++) {
+        s[i] = toupper(s[i]);
+    }
     cout << s << endl;
 
     return 0;
