be49aadb56b973c8c62061d926f453067556dee0
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-09-27 09:19:54 +0000

    autocommit

--- Lectures/Lecture_09_01/main.cpp
+++ Lectures/Lecture_09_01/main.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
-#include <cstring>
-#include <cstdlib>
 #include <string>
+
 using namespace std;
 
 int main() {
-    char s1[100] = "2134";
-    string s2 = "2134";
-
-    cout << atoi(s1) << endl;
-    cout << atoi(s2.c_str()) << endl;
 
 
     return 0;
