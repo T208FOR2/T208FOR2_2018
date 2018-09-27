419e9d39d1d15860a5b8742602d0907c1e13d9f1
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-09-27 09:26:03 +0000

    autocommit

--- Lectures/Lecture_09_01/main.cpp
+++ Lectures/Lecture_09_01/main.cpp
@@ -4,7 +4,14 @@
 using namespace std;
 
 int main() {
+    string s1,s2;
 
+    cin >> s1 >> s2;
+
+    if (s1 == s2) {
+        cout << s1 << " and " << s2 << " are the same" << endl;
+    }
+    cout << s1 + s2 << endl;
 
     return 0;
 }
