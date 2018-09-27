22969a7f234f7a8d616a768f5b386821cbc524b8
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-09-27 08:49:25 +0000

    autocommit

--- Lectures/Lecture_09_01/main.cpp
+++ Lectures/Lecture_09_01/main.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main() {
+    int tictactoe[3][3];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            tictactoe[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << tictactoe[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
