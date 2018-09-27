3f18b3011967b23ab783dd95a174e7364f2ec58e
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-09-27 09:58:54 +0000

    autocommit

--- Lectures/Lecture_09_01/main.cpp
+++ Lectures/Lecture_09_01/main.cpp
@@ -4,6 +4,13 @@
 using namespace std;
 
 int main() {
+    string s, keyword;
+
+    getline(cin, s);
+    cin >> keyword;
+
+    cout << s << endl;
+    cout << s.find(keyword) << endl;
 
 
     return 0;
