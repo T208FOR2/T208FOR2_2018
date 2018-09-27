c93b9c76ce23f2c241f743a3bc5beabe0fd12b6c
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-09-27 09:05:34 +0000

    autocommit

--- Lectures/Lecture_09_01/main.cpp
+++ Lectures/Lecture_09_01/main.cpp
@@ -2,21 +2,15 @@
 using namespace std;
 
 int main() {
-    int tictactoe[3][3];
+    int t[100];
+    char s[100] = "Hello world";
 
-    int k = 0;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            tictactoe[i][j] = k++;
-        }
+    for (int i = 0; i < 100; i++) {
+        t[i] = 0;
     }
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << tictactoe[i][j] << " ";
-        }
-        cout << endl;
-    }
+    cout << s << endl;
+    cout << t << endl;
 
     return 0;
 }
