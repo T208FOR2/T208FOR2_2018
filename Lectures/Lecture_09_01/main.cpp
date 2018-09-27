5a26c23d63f60fd69e0dba9cce2b30d616d8b762
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-09-27 09:36:16 +0000

    autocommit

--- Lectures/Lecture_09_01/main.cpp
+++ Lectures/Lecture_09_01/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
 
 int main() {
+    ifstream theinputfile;
     string s1,s2;
 
-    cin >> s1;
-    getline(cin,s2);
+    theinputfile.open("test.txt");
+    if (theinputfile.fail()) {
+        cout << "aldjgfpioafjsd" << endl;
+        exit(1);
+    }
+
+    getline(theinputfile, s1) >> s2;
 
     cout << s1 << endl;
     cout << s2 << endl;
