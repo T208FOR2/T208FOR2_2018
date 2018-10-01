486b4afbdd29dd98ac7e9d46da68e958ea248200
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-10-01 15:09:42 +0000

    autocommit

--- Lectures/Lecture_10_01/main.cpp
+++ Lectures/Lecture_10_01/main.cpp
@@ -12,9 +12,10 @@ int main()
     getline(cin, s);
     // First we have a name and then a number
 
-    unsigned int pos = s.find_first_of("0123456789");
+    unsigned int pos1 = s.find_first_not_of("0123456789 :");
+    unsigned int pos2 = s.find_first_of("0123456789",pos1+1);
 
-    string name = s.substr(0,pos-1);
+    string name = s.substr(pos1,pos2 - pos1 - 1);
 
     cout << "Hi " << name << endl;
 
