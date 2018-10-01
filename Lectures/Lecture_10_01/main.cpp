4d7e8af80dceba87f1d5bc5d0fda7e2fd70b0b75
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-10-01 14:36:54 +0000

    autocommit

--- Lectures/Lecture_10_01/main.cpp
+++ Lectures/Lecture_10_01/main.cpp
@@ -14,7 +14,7 @@ int main()
     cin >> a;
 
     cout << "01234567890123456789012345678901234567890" << endl;
-    cout << s;
+    cout << s << endl;
     if (s.find(a) < s.length() ) {
         cout << "The word " << a << " starts at index " << s.find(a) << endl;
     }
