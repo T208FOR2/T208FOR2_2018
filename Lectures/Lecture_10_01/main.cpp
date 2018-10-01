2486360b9ea1fcd225b6795bc2cdfe574f62029c
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-10-01 15:11:47 +0000

    autocommit

--- Lectures/Lecture_10_01/main.cpp
+++ Lectures/Lecture_10_01/main.cpp
@@ -4,20 +4,14 @@
 
 using namespace std;
 
-int count_word(string s, string a);
-
 int main()
 {
+    //Replace a with b in the sentence s
     string s;
+    string a,b;
     getline(cin, s);
-    // First we have a name and then a number
+    cin >> a >> b;
 
-    unsigned int pos1 = s.find_first_not_of("0123456789 :");
-    unsigned int pos2 = s.find_first_of("0123456789",pos1+1);
-
-    string name = s.substr(pos1,pos2 - pos1 - 1);
-
-    cout << "Hi " << name << endl;
 
     return 0;
 }
