af73216906d9e34825c42dbe7359832cf82ae0a9
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2018-10-04 09:13:15 +0000

    autocommit

--- Lectures/Lecture_11_01/main.cpp
+++ Lectures/Lecture_11_01/main.cpp
@@ -5,12 +5,14 @@
 
 using namespace std;
 
+const string NOHASHTAG = "no hashtag";
 string getnexthashtag(string texti, unsigned int& pos);
 
 int main() {
     string test = "Lionel and Charles Ortel on Hillary Clinton and Targeting the Unprosecuted Clinton Crime Cartel [VIDEO] #LionelNation #MAGA #KAG2020 @realDonaldTrump @CharlesOrtel #HillaryForPrison #DrainTheSwamp #Midterms2018 #MontanaRally #ClintonCrimeFamily";
 
-
+    unsigned int pos = 0;
+    cout << getnexthashtag(test, pos) << endl;
 
     return 0;
 }
@@ -21,6 +23,9 @@ unsigned int hashstart = texti.find("#");
     if (hashstart < texti.length()) {
         hashend = texti.find_first_not_of("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890", hashstart+1);
         pos = hashend;
-        return texti.substr(hashstart, hashend-hashstart) << endl;
+        return texti.substr(hashstart, hashend-hashstart);
+    }
+    else {
+        return NOHASHTAG;
     }
 }
